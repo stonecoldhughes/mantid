00ddeef45d26dd7cdc28763df3569154bda35181
Owen Arnold <owen.arnold@stfc.ac.uk>
2020-06-17 14:48:10 +0100

    Add to Script Window template

--- qt/applications/workbench/workbench/config/__init__.py
+++ qt/applications/workbench/workbench/config/__init__.py
@@ -54,7 +54,8 @@ if sys.version_info < (3, 0):
 DEFAULT_SCRIPT_CONTENT += "# import mantid algorithms, numpy and matplotlib" + os.linesep + \
                           "from mantid.simpleapi import *" + os.linesep + \
                           "import matplotlib.pyplot as plt" + os.linesep + \
-                          "import numpy as np" + os.linesep + os.linesep
+                          "import numpy as np" + os.linesep + \
+                          "import scipp as sc" + os.linesep + os.linesep
 
 # Flags defining a standard Window
 WINDOW_STANDARD_FLAGS = Qt.WindowFlags(Qt.Window)
